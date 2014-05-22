d5b6271ee846546dbb96bd71dde9d7a540cadde3
scdickson <scdickso@purdue.edu>
2014-05-22 14:01:17 -0600

    Testing data throughput

--- BluetoothCentral/BluetoothCentral/ViewController.h
+++ BluetoothCentral/BluetoothCentral/ViewController.h
@@ -9,11 +9,12 @@
 #import <UIKit/UIKit.h>
 #import <CoreBluetooth/CoreBluetooth.h>
 
-@interface ViewController : UIViewController <CBCentralManagerDelegate, CBPeripheralDelegate>
+@interface ViewController : UIViewController <CBCentralManagerDelegate, CBPeripheralDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (strong, nonatomic) CBCentralManager *manager;
 @property (strong, nonatomic) CBPeripheral *peripheral;
 @property (strong, nonatomic) CBCharacteristic *peripheralCharacteristic;
 @property (strong, nonatomic) CBCharacteristic *writebackCharacteristic;
+@property (strong, nonatomic) CBCharacteristic *dataCharacteristic;
 @property (nonatomic, readwrite) NSInteger dataIndex;
 @property (strong, nonatomic) NSData *data;
 @end
